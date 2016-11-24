fc1c4b2d56f722d0049277b580d079c94fb25f39
Elisha <kramer1346@gmail.com>
2016-11-24 16:27:13 +0200

    Add main project files

--- People.js
+++ People.js
@@ -1,3 +1,15 @@
 app.controller('PeopleCtrl', function($scope, PeopleService) {
   $scope.People = PeopleService.People;
+
+
+  $scope.stuff = [];
+
+	$scope.pushToArray = function(){
+    $scope.stuff.push($scope.userText);
+    console.log($scope.stuff);
+
+  $scope.animate = true;
+};
+
+
 });
